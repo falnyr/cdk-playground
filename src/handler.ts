c05d5192b4be12055994175b4161b258df4f70a2
Jan Richter <jan.richter@actionstep.com>
2020-10-15 09:03:56 +1300

    chore: add webpack

--- src/handler.ts
+++ src/handler.ts
@@ -2,6 +2,7 @@ import {toSvg} from "jdenticon";
 
 export const handler = async (event: any = {}) : Promise <any> => {
   console.log(event);
+  // TODO: https://webpack.js.org/
   const svgString = toSvg("Suella", 300)
 
   return { statusCode: 200, body: svgString };
